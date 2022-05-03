4042ecbd71de689d3ebb910965e5337ce8604200
Alex Mitchell <alex.d.mitchell@protonmail.com>
2022-05-03 18:29:56 +0100

    Add useReducer and buttons to toggle play state

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,61 @@
 import './App.css'
 
+import React, { useReducer } from 'react'
+
 import { Grid } from './components/Grid/Grid'
 import { Navbar } from './components/Navbar/Navbar'
 
+type Grid = (0 | 1)[][]
+
+const grid: Grid = [
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 1, 1, 0, 0, 0, 0],
+  [0, 0, 0, 0, 1, 1, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+]
+
+type State = {
+  isPlaying: boolean
+  grid: Grid
+}
+
+type Action = {
+  type: 'toggle-is-playing'
+}
+
+function reducer(state: State, action: Action): State {
+  switch (action.type) {
+    case 'toggle-is-playing':
+      return {
+        ...state,
+        isPlaying: !state.isPlaying
+      }
+  }
+}
+
+const initialState: State = {
+  isPlaying: false,
+  grid
+}
+
+const useGameOfLife = () => {
+  const [state, dispatch] = React.useReducer(reducer, initialState)
+  return { state, dispatch }
+}
+
 export function App() {
+  const { state, dispatch } = useGameOfLife()
+  const toggleIsPlaying = () => dispatch({ type: 'toggle-is-playing' })
+
   return (
     <div className={'App'}>
-      <Navbar />
+      <Navbar isPlaying={state.isPlaying} toggleIsPlaying={toggleIsPlaying} />
       <Grid />
     </div>
   )
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,14 +1,24 @@
 import './Navbar.css'
 
-export const Navbar = () => (
+interface Props {
+  isPlaying: boolean
+  toggleIsPlaying: () => void
+}
+
+export const Navbar = ({ isPlaying, toggleIsPlaying }: Props) => (
   <div className={'navbar'}>
     <h1>Game of Life</h1>
-    <a
-      className={'navbar_link'}
-      href={`https://en.wikipedia.org/wiki/Conway's_Game_of_Life`}
-      target={'_blank'}
-      rel={'noreferrer'}>
-      Info
-    </a>
+    <div className={'navbar_actions'}>
+      <a
+        className={'navbar_link'}
+        href={`https://en.wikipedia.org/wiki/Conway's_Game_of_Life`}
+        target={'_blank'}
+        rel={'noreferrer'}>
+        Info
+      </a>
+      <button className={'navbar_button'} onClick={toggleIsPlaying}>
+        {isPlaying ? 'Stop' : 'Start'}
+      </button>
+    </div>
   </div>
 )
