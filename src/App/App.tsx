153ea9f72ce720c0abaa69e151ffb2ebdb363bc8
Alex Mitchell <alex.d.mitchell@protonmail.com>
2022-05-09 21:46:55 +0100

    Add light / dark mode

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -26,61 +26,95 @@ function AppContent() {
     toggleCellFill
   } = useGameOfLife()
 
-  const { color, setColor } = React.useContext(ColorContext)
+  const { color, setColor, secondaryColor, setSecondaryColor } =
+    React.useContext(ColorContext)
+
+  const buttonStyle = {
+    backgroundColor: color,
+    borderColor: color,
+    color: `${secondaryColor}cc`
+  }
+
+  const outlineButtonStyle = {
+    color,
+    borderColor: color
+  }
+
+  const isDark = secondaryColor === '#000000'
 
   if (state.screen === 'settings') {
     return (
-      <div className={'App'} style={{ backgroundColor: `${color}33` }}>
-        <Navbar>
-          <button
-            className={'button'}
-            style={{ backgroundColor: color, borderColor: color }}
-            onClick={() => dispatch({ type: 'navigate', screen: 'game' })}>
-            Back
-          </button>
-        </Navbar>
-        <div className={'settings'}>
-          <div className={'settings_item'}>
-            <Slider
-              label={'Speed'}
-              value={-state.generationDuration}
-              onChange={(value) => {
-                dispatch({
-                  type: 'update-generation-duration',
-                  value: -value
-                })
-              }}
-              min={-3200}
-              max={-32}
-            />
-          </div>
-
-          <div className={'settings_item'}>
-            <Slider
-              label={'Cell size'}
-              value={state.cellSize}
-              onChange={(value) => {
-                dispatch({ type: 'update-cell-size', value })
-              }}
-              min={5}
-              max={50}
-            />
-          </div>
-
-          <div className={'settings_item'}>
-            <ColorPicker />
-          </div>
-
-          <div className={'settings_item'}>
+      <div className={'App'} style={{ backgroundColor: secondaryColor }}>
+        <div className={'App'} style={{ backgroundColor: `${color}33` }}>
+          <Navbar>
             <button
-              className={'button button_large'}
-              style={{ backgroundColor: color, borderColor: color }}
-              onClick={() => {
-                setColor('#6b7502')
-                dispatch({ type: 'restore-default-settings' })
-              }}>
-              Restore defaults
+              className={'button'}
+              style={buttonStyle}
+              onClick={() => dispatch({ type: 'navigate', screen: 'game' })}>
+              Back
             </button>
+          </Navbar>
+          <div className={'settings'}>
+            <div className={'settings_item'}>
+              <Slider
+                label={'Speed'}
+                value={-state.generationDuration}
+                onChange={(value) => {
+                  dispatch({
+                    type: 'update-generation-duration',
+                    value: -value
+                  })
+                }}
+                min={-3200}
+                max={-32}
+              />
+            </div>
+
+            <div className={'settings_item'}>
+              <Slider
+                label={'Cell size'}
+                value={state.cellSize}
+                onChange={(value) => {
+                  dispatch({ type: 'update-cell-size', value })
+                }}
+                min={5}
+                max={50}
+              />
+            </div>
+
+            <div className={'settings_item'}>
+              <ColorPicker />
+            </div>
+
+            <div className={'settings_item'}>
+              <button
+                className={`button no_margin ${
+                  !isDark ? 'button_outline' : ''
+                }`}
+                style={!isDark ? outlineButtonStyle : buttonStyle}
+                onClick={() => setSecondaryColor('#000000')}>
+                Dark
+              </button>
+              <button
+                className={`button no_margin ${isDark ? 'button_outline' : ''}`}
+                style={isDark ? outlineButtonStyle : buttonStyle}
+                onClick={() => setSecondaryColor('#ffffff')}>
+                Light
+              </button>
+            </div>
+
+            <div className={'settings_item'}>
+              <button
+                className={'button button_large'}
+                style={buttonStyle}
+                onClick={() => {
+                  setColor('#6b7502')
+                  setSecondaryColor('#000000')
+                  dispatch({ type: 'restore-default-settings' })
+                }}>
+                Restore defaults
+              </button>
+            </div>
           </div>
         </div>
       </div>
@@ -88,64 +122,62 @@ function AppContent() {
   }
 
   return (
-    <div className={'App'} style={{ backgroundColor: `${color}33` }}>
-      <Navbar>
-        <a
-          className={'navbar_link'}
-          style={{ color }}
-          href={`https://en.wikipedia.org/wiki/Conway's_Game_of_Life`}
-          target={'_blank'}
-          rel={'noreferrer'}>
-          Info
-        </a>
+    <div className={'App'} style={{ backgroundColor: secondaryColor }}>
+      <div className={'App'} style={{ backgroundColor: `${color}33` }}>
+        <Navbar>
+          <a
+            className={'navbar_link'}
+            style={{ color }}
+            href={`https://en.wikipedia.org/wiki/Conway's_Game_of_Life`}
+            target={'_blank'}
+            rel={'noreferrer'}>
+            Info
+          </a>
 
-        <button
-          className={`button ${state.isPlaying ? 'button_outline' : ''}`}
-          style={
-            state.isPlaying
-              ? { color, borderColor: color }
-              : { backgroundColor: color, borderColor: color }
-          }
-          onClick={toggleIsPlaying}>
-          {state.isPlaying ? 'Stop' : 'Start'}
-        </button>
+          <button
+            className={`button ${state.isPlaying ? 'button_outline' : ''}`}
+            style={state.isPlaying ? outlineButtonStyle : buttonStyle}
+            onClick={toggleIsPlaying}>
+            {state.isPlaying ? 'Stop' : 'Start'}
+          </button>
 
-        <button
-          className={'button'}
-          style={{ backgroundColor: color, borderColor: color }}
-          onClick={() => {
-            const grid = createFullScreenGrid({
-              cellSize: state.cellSize,
-              randomise: true
-            })
-            dispatch({ type: 'set-grid', grid, keepPlaying: false })
-          }}>
-          Randomise
-        </button>
+          <button
+            className={'button'}
+            style={buttonStyle}
+            onClick={() => {
+              const grid = createFullScreenGrid({
+                cellSize: state.cellSize,
+                randomise: true
+              })
+              dispatch({ type: 'set-grid', grid, keepPlaying: false })
+            }}>
+            Randomise
+          </button>
 
-        <button
-          className={'button'}
-          style={{ backgroundColor: color, borderColor: color }}
-          onClick={() => {
-            const grid = createFullScreenGrid({ cellSize: state.cellSize })
-            dispatch({ type: 'set-grid', grid, keepPlaying: false })
-          }}>
-          Clear
-        </button>
+          <button
+            className={'button'}
+            style={buttonStyle}
+            onClick={() => {
+              const grid = createFullScreenGrid({ cellSize: state.cellSize })
+              dispatch({ type: 'set-grid', grid, keepPlaying: false })
+            }}>
+            Clear
+          </button>
 
-        <button
-          className={'button settings_button'}
-          style={{ backgroundColor: color, borderColor: color }}
-          onClick={() => dispatch({ type: 'navigate', screen: 'settings' })}>
-          &#8942;
-        </button>
-      </Navbar>
+          <button
+            className={'button settings_button'}
+            style={buttonStyle}
+            onClick={() => dispatch({ type: 'navigate', screen: 'settings' })}>
+            &#8942;
+          </button>
+        </Navbar>
 
-      <Grid
-        grid={state.grid}
-        toggleCellFill={toggleCellFill}
-        cellSize={state.cellSize}
-      />
+        <Grid
+          grid={state.grid}
+          toggleCellFill={toggleCellFill}
+          cellSize={state.cellSize}
+        />
+      </div>
     </div>
   )
 }
--- src/App/context/ColorProvider.tsx
+++ src/App/context/ColorProvider.tsx
@@ -1,22 +1,39 @@
 import React from 'react'
 
-const context = {
-  color: '#6b7502',
-  setColor: (value: string) => undefined
+type monochrome = '#000000' | '#ffffff'
+
+type Context = {
+  color: string
+  secondaryColor: monochrome
+  setColor: (_value: string) => void
+  setSecondaryColor: (_value: monochrome) => void
 }
 
-export const ColorContext = React.createContext(context)
+const context: Context = {
+  color: '#6b7502',
+  secondaryColor: '#000000',
+  setColor: (_value: string) => {
+    //
+  },
+  setSecondaryColor: (_value: monochrome) => {
+    //
+  }
+}
+
+export const ColorContext = React.createContext<Context>(context)
 
 export const ColorProvider = ({ children }: { children: React.ReactNode }) => {
   const [color, setColor] = React.useState('#6b7502')
+  const [secondaryColor, setSecondaryColor] =
+    React.useState<monochrome>('#000000')
+
   return (
     <ColorContext.Provider
       value={{
         color,
-        setColor: (value) => {
-          setColor(value)
-          return undefined
-        }
+        secondaryColor,
+        setColor: (value) => setColor(value),
+        setSecondaryColor: (value) => setSecondaryColor(value)
       }}>
       {children}
     </ColorContext.Provider>
