652a144508e06d416216223dbbf75171908bf640
Alex Mitchell <alex.d.mitchell@protonmail.com>
2022-05-11 21:20:34 +0100

    Add side drawer to navbar

--- src/App/components/Navbar/Navbar.tsx
+++ src/App/components/Navbar/Navbar.tsx
@@ -9,12 +9,54 @@ interface Props {
 }
 
 export const Navbar = ({ children }: Props) => {
-  const { color } = React.useContext(ColorContext)
+  const { color, secondaryColor } = React.useContext(ColorContext)
+
+  const [showDrawer, setShowDrawer] = React.useState(false)
+  const toggleDrawer = () => setShowDrawer((current) => !current)
+
+  const buttonStyle = {
+    backgroundColor: color,
+    borderColor: color,
+    color: `${secondaryColor}cc`
+  }
 
   return (
     <div className={'navbar'}>
       <h1 style={{ color }}>Game of Life</h1>
       <div className={'navbar_actions'}>{children}</div>
+      <button
+        className={'button side_drawer_button'}
+        style={buttonStyle}
+        onClick={toggleDrawer}>
+        <div className={'burger'}>
+          <div className={'burger_bar'} />
+          <div className={'burger_bar'} />
+          <div className={'burger_bar'} />
+        </div>
+      </button>
+      {showDrawer && (
+        <>
+          <button className={'backdrop'} onClick={toggleDrawer} />
+          <div
+            className={'side_drawer'}
+            style={{ backgroundColor: secondaryColor }}>
+            <div
+              className={'side_drawer_content'}
+              style={{
+                backgroundColor: `${color}33`,
+                borderColor: `${color}1a`
+              }}>
+              <button
+                className={'button side_drawer_button'}
+                style={buttonStyle}
+                onClick={toggleDrawer}>
+                <div className={'cross'} />
+              </button>
+              {children}
+            </div>
+          </div>
+        </>
+      )}
     </div>
   )
 }
